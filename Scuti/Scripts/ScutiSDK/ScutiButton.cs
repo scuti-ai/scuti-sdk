086a09f8910ca69a4ce53b6ddd7c94c3780c83ce
Mark Grossnickle <mark.grossnickle@gmail.com>
2021-05-27 15:35:56 -0600

    Notification Icon wired

--- Scuti/Scripts/ScutiSDK/ScutiButton.cs
+++ Scuti/Scripts/ScutiSDK/ScutiButton.cs
@@ -1,10 +1,61 @@
 ﻿using UnityEngine;
 using Scuti;
+using Scuti.Net;
+using System;
 
 public class ScutiButton : MonoBehaviour
 {
+    public GameObject NotificationIcon;
+
+    private void Awake()
+    {
+        NotificationIcon.SetActive(true);
+    }
+
+    public void Start()
+    {
+        if(ScutiNetClient.Instance != null)
+        {
+            if (ScutiNetClient.Instance.IsInitialized && ScutiNetClient.Instance.IsAuthenticated)
+            {
+                CheckRewards();
+            }
+            else
+            {
+                ScutiNetClient.Instance.OnAuthenticated += CheckRewards;
+            }
+        }  
+    }
+
     public void OnClick()
     { 
         ScutiSDK.Instance.LoadUI();
+        NotificationIcon.SetActive(false);
     }
+
+    private async void CheckRewards()
+    {
+        var rewards = await ScutiAPI.GetRewards(); 
+        foreach(var reward in rewards)
+        {
+            if(reward.Activated==false)
+            {
+                NotificationIcon.SetActive(true);
+                return;
+            }
+            Debug.Log(reward.Activated);
+        }
+        NotificationIcon.SetActive(false);
+    }
+
+
+    private void OnDestroy()
+    {
+        if (ScutiNetClient.Instance != null)
+        {
+            ScutiNetClient.Instance.OnAuthenticated -= CheckRewards;
+        }
+    }
+
+
 }
