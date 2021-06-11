833484b1382a2d334f5dd44bf7ca287b6a61e238
Mark Grossnickle <markgrossnickle@users.noreply.github.com>
2021-06-11 10:48:26 -0600

    Revert screen x

--- Scuti/Scripts/Core/ScreenX.cs
+++ Scuti/Scripts/Core/ScreenX.cs
@@ -1,12 +1,16 @@
 ﻿using UnityEngine;
 
-namespace Scuti {
-    public class ScreenX {
-        public static int Height {
+namespace Scuti
+{
+    public class ScreenX
+    {
+        public static int Height
+        {
             get { return (int)GetWindowSize().y; }
         }
 
-        public static int Width {
+        public static int Width
+        {
             get { return (int)GetWindowSize().x; }
         }
 
@@ -15,7 +19,8 @@ namespace Scuti {
         /// In the build, this returns Screen.width, Screen.height
         /// In the editor, this returns the size of the Game Window using reflection
         /// </summary>
-        public static Vector2 GetWindowSize() {
+        public static Vector2 GetWindowSize()
+        {
             // Screen.width and Screen.height sometimes return the dimensions of the inspector
             // window when Screen.width originates from a ContextMenu or Button attribute
             // We use reflection to get the actual dimensions. During runtime we simply use Screen again
@@ -23,12 +28,12 @@ namespace Scuti {
             System.Type T = System.Type.GetType("UnityEditor.GameView,UnityEditor");
             System.Reflection.MethodInfo GetSizeOfMainGameView = T.GetMethod("GetSizeOfMainGameView", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static);
             System.Object Res = GetSizeOfMainGameView.Invoke(null, null);
-            var result = (Vector2)Res; 
+            var result = (Vector2)Res;
             return result;
 #else
             return new Vector2(
-                Camera.main.pixelWidth,
-                Camera.main.pixelHeight
+                Screen.width,
+                Screen.height
             );
 #endif
         }
@@ -38,7 +43,8 @@ namespace Scuti {
         /// A is the size of the window the game is running in. This can be in windowed or editor mode
         /// B is the resolution that the application is set to
         /// </summary>
-        public static Vector2 VectorToResolution(Vector2 res) {
+        public static Vector2 VectorToResolution(Vector2 res)
+        {
             var windowSize = GetWindowSize();
             var scaleFactor = new Vector2(
                 (Screen.currentResolution.width / windowSize.x),
@@ -56,7 +62,8 @@ namespace Scuti {
         /// A is the resolution that the application is set to
         /// B is the size of the window the game is running in. This can be in windowed or editor mode
         /// </summary>
-        public static Vector2 VectorToWindow(Vector2 res) {
+        public static Vector2 VectorToWindow(Vector2 res)
+        {
             var windowSize = GetWindowSize();
             var scaleFactor = new Vector2(
                 (Screen.currentResolution.width / windowSize.x),
