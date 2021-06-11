bd6dcb707d8692b65a11bb283841d7737dc5981e
Mark Grossnickle <markgrossnickle@users.noreply.github.com>
2021-06-11 10:38:43 -0600

    Revert to beginning

--- Scuti/Scripts/Utils/CustomScaling.cs
+++ Scuti/Scripts/Utils/CustomScaling.cs
@@ -1,59 +1,45 @@
-﻿using UnityEngine;
+﻿
+using Scuti;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace Scuti.UI
 {
-	[ExecuteInEditMode]
+    [ExecuteInEditMode]
     public class CustomScaling : MonoBehaviour
     {
-        [HideInInspector][SerializeField] private CanvasScaler canvas;
+        [SerializeField] private CanvasScaler canvas;
         [SerializeField] private float scale = 0.56f;
         private float _aspectRatio = -1;
-        private bool _orientation = true;
 
-        private float AspectRatio
+        int _count = 0;
+
+        public float AspectRatio
         {
             get
             {
-	            {
-		            _aspectRatio = Orientation
-			            ? (float) ScreenX.Height / ScreenX.Width
-			            : ScreenX.Width / (float) ScreenX.Height;
-	            }
+                {
+                    _aspectRatio = ScreenX.Width / (float)ScreenX.Height;
+                }
                 return _aspectRatio;
             }
         }
 
-        private bool Orientation
-        {
-            get
-            {
-	            {
-		            _orientation = Screen.orientation == ScreenOrientation.Landscape;
-                    #if UNITY_EDITOR
-                    _orientation = ScreenX.Width > ScreenX.Height;
-                    #endif
-	            }
-                return _orientation;
-            }
-        }
 
-        private void Update()
+        private void Start()
         {
             RefreshScale();
         }
-
-#if UNITY_EDITOR
-	    private void OnGUI()
+        private void Update()
         {
-	        RefreshScale();
+            if (_count % 60 == 0)
+                RefreshScale();
+            _count++;
         }
-#endif
-	    private void RefreshScale()
+
+        private void RefreshScale()
         {
-	        canvas.referenceResolution = Orientation ? new Vector2(1080, 1920) : new Vector2(1920, 1080);
-	        var dynamicScale = Orientation ? 1 : scale;
-            canvas.matchWidthOrHeight = AspectRatio < 1.7f ? dynamicScale : 1;
+            canvas.matchWidthOrHeight = AspectRatio < 1.7f ? scale : 1;
         }
 
         private void OnValidate()
