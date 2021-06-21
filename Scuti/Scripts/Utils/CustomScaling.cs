f4ebb4f99106461428fbdab52b872de5af222315
Maciej Górnicki <maciej@gornicki.me>
2021-06-21 12:41:52 +0300

    updated custom scaling with changes for portrait that were missing

--- Scuti/Scripts/Utils/CustomScaling.cs
+++ Scuti/Scripts/Utils/CustomScaling.cs
@@ -1,45 +1,59 @@
-﻿
-using Scuti;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 namespace Scuti.UI
 {
-    [ExecuteInEditMode]
+	[ExecuteInEditMode]
     public class CustomScaling : MonoBehaviour
     {
-        [SerializeField] private CanvasScaler canvas;
+        [HideInInspector][SerializeField] private CanvasScaler canvas;
         [SerializeField] private float scale = 0.56f;
         private float _aspectRatio = -1;
+        private bool _landscape = true;
 
-        int _count = 0;
-
-        public float AspectRatio
+        private float AspectRatio
         {
             get
             {
-                {
-                    _aspectRatio = ScreenX.Width / (float)ScreenX.Height;
-                }
+	            {
+		            _aspectRatio = Landscape
+			            ? (float) Camera.main.pixelWidth / Camera.main.pixelHeight
+			            : Camera.main.pixelHeight / (float) Camera.main.pixelWidth;
+	            }
                 return _aspectRatio;
             }
         }
 
+        private bool Landscape
+        {
+            get
+            {
+	            {
+		            _landscape = Screen.orientation == ScreenOrientation.Landscape || Screen.orientation == ScreenOrientation.LandscapeLeft || Screen.orientation == ScreenOrientation.LandscapeRight;
+                    #if UNITY_EDITOR
+		            _landscape = Camera.main.pixelWidth > Camera.main.pixelHeight;
+                    #endif
+	            }
+                return _landscape;
+            }
+        }
 
-        private void Start()
+        private void Update()
         {
             RefreshScale();
         }
-        private void Update()
-        {
-            if (_count % 60 == 0)
-                RefreshScale();
-            _count++;
-        }
 
-        private void RefreshScale()
+#if UNITY_EDITOR
+	    private void OnGUI()
         {
-            canvas.matchWidthOrHeight = AspectRatio < 1.7f ? scale : 1;
+	        RefreshScale();
+        }
+#endif
+	    private void RefreshScale()
+        {
+	        canvas.referenceResolution = Landscape ? new Vector2(1920, 1080) : new Vector2(1080, 1920);
+	        var dynamicScale = scale;
+            canvas.matchWidthOrHeight = AspectRatio < 1.7f ? dynamicScale : 1;
         }
 
         private void OnValidate()
